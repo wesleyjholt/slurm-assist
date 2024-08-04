d9a4adb0666c3b6f8af12abaf24d93e770c1fe0e
Wesley Holt <wesleyjholt@gmail.com>
2024-08-03 22:26:58 -0400

    add hpc-ignore functionality

--- hpc-templates/many-independent-jobs/hpc/push_to_remote.sh
+++ hpc-templates/many-independent-jobs/hpc/push_to_remote.sh
@@ -5,4 +5,13 @@ PATH_TO_REMOTE=/scratch/${CLUSTER}/${USERNAME}/${PROJECT_NAME}/v${CODE_VERSION}
 # Create path to remote if it doesn't exist
 ssh ${USERNAME}@${CLUSTER}.rcac.purdue.edu "mkdir -p ${PATH_TO_REMOTE}"
 
-scp -r ./ ${USERNAME}@${CLUSTER}.rcac.purdue.edu:${PATH_TO_REMOTE}
+if [ -f "hpc-ignore" ]; then
+    # Exclude files listed in hpc-ignore
+    find . | grep -v '^.$' | grep -v -f hpc-ignore \
+    | xargs tar cf - \
+    | ssh ${USERNAME}@${CLUSTER}.rcac.purdue.edu tar xf - -C ${PATH_TO_REMOTE}
+else
+    find . \
+    | xargs tar cf - \
+    | ssh ${USERNAME}@${CLUSTER}.rcac.purdue.edu tar xf - -C ${PATH_TO_REMOTE}
+fi
