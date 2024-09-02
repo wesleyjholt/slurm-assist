7d86dd217f7898a400edfed6f248d2dc4dbbb9bf
Wesley Holt <wesleyjholt@gmail.com>
2024-09-02 13:15:48 -0400

    delete extra dir

--- src/slurm_assist/many_small_jobs_pyslurm/parse_image_from_args.py
+++
@@ -1,8 +0,0 @@
-# This script grabs the container image and passes it on first, followed by all other arguments.
-
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--container-image', type=str)
-args, unknown_args = parser.parse_known_args()
-print(args.container_image)
